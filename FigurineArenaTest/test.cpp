b88d4f4c054cba62283cbc6a47402dd3f0a9fe9a
memequeen2015 <notaverageenough@gmail.com>
2015-09-04 15:43:40 +1200

    Asma's Notes

--- FigurineArenaTest/test.cpp
+++ FigurineArenaTest/test.cpp
@@ -3,3 +3,6 @@
 //SO DANK
 
 //Ohmahgod that nick so cool
+
+//PRODUCT BACKLOG - ADD "As a Player, I want... e.g. to have a menu I can easily navigate
+//PRODUCT BACKLOG - ADD GDD + TDD + JOURNALS ETC.
